2ec636e4aefed233d172722bcb6e6a87d90913de
Liam Pilarski <liampilarski@gmail.com>
2024-12-05 11:43:20 -0500

    Add more paramterization to train.py

--- train.py
+++ train.py
@@ -7,13 +7,6 @@ from model import StockPredictor
 import argparse
 
 
-BATCH_SIZE=16
-NEUTRAL_WINDOW=0.005
-PRINT_EVERY=1
-
-# for testing purposes only. Set to None otherwise
-EARLY_EXIT=1
-
 def convert_to_class_indices(labels, neutral_window):
     class_labels = torch.empty(labels.size(0), device=labels.device, dtype=torch.long)
     class_labels[labels < -neutral_window] = 0  # Negative class
@@ -23,11 +16,12 @@ def convert_to_class_indices(labels, neutral_window):
     return class_labels
 
 
-def save_model(epoch, model, optimizer, model_save_path):
+def save_model(epoch, model, optimizer, neutral_window, model_save_path):
     checkpoint = {
         'epoch': epoch,
         'model_state_dict': model.state_dict(),
         'optimizer_state_dict': optimizer.state_dict(),
+        'neutral_window': neutral_window
     }
 
     torch.save(checkpoint, model_save_path)
@@ -37,7 +31,8 @@ def load_model(model_save_path, model, optimizer):
     model.load_state_dict(checkpoint['model_state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
     current_epoch = checkpoint['epoch']
-    return model, optimizer, current_epoch
+    neutral_window = checkpoint['neutral_window']
+    return model, optimizer, current_epoch, neutral_window
 
 
 
@@ -64,9 +59,9 @@ def train_epoch(model, data_loader, loss_function, optimizer, device, epoch):
         optimizer.step()
 
         if batch_num % PRINT_EVERY == 0:
-            print("EPOCH {}: BATCH {}/{}, LOSS: {:.4f}".format(epoch, batch_num, len(data_loader), loss.item()))
+            print("EPOCH {}: BATCH {}/{}, LOSS: {:.4f}".format(epoch, batch_num, len(data_loader), loss.item() / input_ids.size(0)))
 
-        total_loss += loss.item() * input_ids.size(0)
+        total_loss += loss.item()
         total_examples += input_ids.size(0)
 
         preds = torch.argmax(logits, dim=1).cpu().numpy()
@@ -99,7 +94,7 @@ def evaluate(model, data_loader, loss_function, device):
             logits = model(input_ids=input_ids, attention_mask=attention_mask, financial_data=financial_data)
 
             loss = loss_function(logits, labels)
-            total_loss += loss.item() * input_ids.size(0)
+            total_loss += loss.item()
             total_examples += input_ids.size(0)
 
             preds = torch.argmax(logits, dim=1).cpu().numpy()
@@ -126,9 +121,19 @@ if __name__ == "__main__":
         default="analyst_ratings_processed_filtered_combined.csv",
         help="Path to the dataset file."
     )
+    parser.add_argument("--batch_size", type=int, default=16, help="Batch size for training.")
+    parser.add_argument("--neutral_window", type=float, default=0.005, help="Neutral window for label classification.")
+    parser.add_argument("--print_every", type=int, default=1, help="Print loss every n batches.")
+    parser.add_argument("--early_exit", type=int, default=None, help="Exit training after n batches for debugging.")
+    parser.add_argument("--epochs", type=int, default=0, help="Number of epochs")
     args = parser.parse_args()
 
     file_path = args.file_path
+    BATCH_SIZE = args.batch_size
+    NEUTRAL_WINDOW = args.neutral_window
+    PRINT_EVERY = args.print_every
+    EARLY_EXIT = args.early_exit
+    EPOCHS = args.epochs
 
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     dataset = FinancialDataset(file_path, tokenizer)
@@ -146,19 +151,18 @@ if __name__ == "__main__":
     test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
     model = StockPredictor()
-    loss_fn = torch.nn.CrossEntropyLoss()
+    loss_fn = torch.nn.CrossEntropyLoss(reduction="sum")
     learning_rate = 1e-4
     optimizer = AdamW(model.parameters(), lr=learning_rate)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    epochs = 5
     epoch = 0
-    for i in range(epochs):
+    for i in range(EPOCHS):
         epoch += 1
         train_epoch(model, train_dataloader, loss_fn, optimizer, device, epoch)
         avg_loss, accuracy, f1 = evaluate(model, test_dataloader, loss_fn, device)
-        print("EPOCH {}\nAverage Loss: {}\nAccuracy: {}\nF1 Score: {}".format(epoch, avg_loss, accuracy, f1))
+        print("EPOCH {}\nAverage Loss: {}\nAccuracy: {}\nF1 Score: {}\n-----------------------------------\n".format(epoch, avg_loss, accuracy, f1))
+        save_model(epoch, model, optimizer, NEUTRAL_WINDOW, 'model_checkpoint.pth')
 
-    save_model(epoch, model, optimizer, 'model_checkpoint.pth')
-    model, optimizer, epoch = load_model('model_checkpoint.pth', model, optimizer)
-    print(epoch)
+    model, optimizer, epoch, neutral_window = load_model('model_checkpoint.pth', model, optimizer)
+    print(neutral_window)
